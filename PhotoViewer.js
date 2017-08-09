4ec6bda1848eb2b9d6ad429a749d5f02d032ceeb
Narendra N Shetty <mailnarendra15@gmail.com>
2017-08-09 22:08:30 +0200

    did reverse animation on the image when pressed back

--- PhotoViewer.js
+++ PhotoViewer.js
@@ -16,60 +16,75 @@ const maxWidth = Dimensions.get('window').width;
 const maxHeight = Dimensions.get('window').height;
 
 class DetailView extends React.Component {
-  render() {
-    const { photo, onClose, openProgress } = this.props;
+  state = {
+    localPhoto: null
+  };
 
-    return (
-      <Animated.View style={[StyleSheet.absoluteFill, {}]}>
-        <Animated.Image
-          ref={r => (this._openingImageRef = r)}
-          source={photo.source}
-          style={{
-            width: maxWidth,
-            height: 300,
-            opacity: openProgress.interpolate({
-              inputRange: [0, 0.99, 0.995],
-              outputRange: [0, 0, 1]
-            })
-          }}
-        />
-        <Animated.View
-          style={[
-            styles.body,
-            {
-              opacity: openProgress,
-              backgroundColor: '#fff',
-              transform: [
-                {
-                  translateY: openProgress.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [100, 0]
-                  })
-                }
-              ]
-            }
-          ]}
-        >
-          <Text style={styles.title}>
-            - {photo.postedBy}
-          </Text>
-          <Text style={styles.description}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </Text>
+  componentWillReceiveProps(nextProps) {
+    const { photo } = nextProps;
+    if (photo) {
+      this.setState({ localPhoto: photo });
+    }
+  }
+
+  render() {
+    const { onClose, openProgress } = this.props;
+    const { localPhoto } = this.state;
+    if (localPhoto) {
+      return (
+        <Animated.View style={[StyleSheet.absoluteFill, {}]}>
+          <Animated.Image
+            ref={r => (this._openingImageRef = r)}
+            source={localPhoto.source}
+            style={{
+              width: maxWidth,
+              height: 300,
+              opacity: openProgress.interpolate({
+                inputRange: [0, 0.99, 0.995],
+                outputRange: [0, 0, 1]
+              })
+            }}
+          />
+          <Animated.View
+            style={[
+              styles.body,
+              {
+                opacity: openProgress,
+                backgroundColor: '#fff',
+                transform: [
+                  {
+                    translateY: openProgress.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [100, 0]
+                    })
+                  }
+                ]
+              }
+            ]}
+          >
+            <Text style={styles.title}>
+              - {localPhoto.postedBy}
+            </Text>
+            <Text style={styles.description}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </Text>
+          </Animated.View>
+          <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+            <Text style={styles.closeText}>Close</Text>
+          </TouchableOpacity>
         </Animated.View>
-        <TouchableOpacity onPress={onClose} style={styles.closeButton}>
-          <Text style={styles.closeText}>Close</Text>
-        </TouchableOpacity>
-      </Animated.View>
-    );
+      );
+    }
+    return <View />;
   }
 }
 
@@ -103,15 +118,16 @@ class Transition extends React.Component {
               height,
               pageX,
               pageY
-            }
+            },
+            photo
           });
         });
     }
   }
 
   render() {
-    const { photo, openProgress } = this.props;
-    const { destinationDimension, sourceDimension } = this.state;
+    const { openProgress } = this.props;
+    const { destinationDimension, sourceDimension, photo } = this.state;
 
     if (photo) {
       let destRightDimension = {
@@ -267,14 +283,18 @@ export default class PhotoViewer extends React.Component {
         toValue: 1,
         duration: 3000,
         useNativeDriver: true
-      }).start(() => {
-        this.setState({ openProgress: new Animated.Value(0) });
-      });
+      }).start();
     });
   };
 
   close = () => {
-    this.setState({ photo: null }, () => {});
+    this.setState({ photo: null }, () => {
+      Animated.timing(this.state.openProgress, {
+        toValue: 0,
+        duration: 3000,
+        useNativeDriver: true
+      }).start();
+    });
   };
 
   render() {
@@ -282,17 +302,17 @@ export default class PhotoViewer extends React.Component {
     return (
       <View style={{ flex: 1 }}>
         {this.props.renderContent({ onPhotoOpen: this.open })}
+
+        <DetailView
+          photo={photo}
+          onClose={this.close}
+          openProgress={openProgress}
+        />
         <Transition
           openProgress={openProgress}
           photo={photo}
           sourceImageRefs={this._images}
         />
-        {photo &&
-          <DetailView
-            photo={photo}
-            onClose={this.close}
-            openProgress={openProgress}
-          />}
       </View>
     );
   }
